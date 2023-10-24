79588004f93021d21535f3770d1a27bab91966b7
BrokenShell <webmaster@sharpdesigndigital.com>
2023-10-24 09:56:47 -0700

    filename update

--- app/main.py
+++ app/main.py
@@ -4,10 +4,11 @@ from tempfile import NamedTemporaryFile
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
+from fastapi.exceptions import HTTPException
 
 from app.transform import md_file_to_html_file
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 API = FastAPI(
     title='Markdown to HTML',
     version=VERSION,
@@ -30,10 +31,12 @@ async def version():
 
 @API.post("/process", tags=["MD To HTML"])
 async def process(file: UploadFile = File(...)):
+    if not file.filename.endswith(".md"):
+        raise HTTPException(500)
     temp_file = NamedTemporaryFile(delete=False)
     with temp_file as buffer:
         shutil.copyfileobj(file.file, buffer)
-    filename = file.filename.split(".")[0]
+    filename = file.filename.replace(".md", "")
     html_file = filename + ".html"
     md_file_to_html_file(temp_file.name, html_file)
     return FileResponse(
